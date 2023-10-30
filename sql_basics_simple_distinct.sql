073f53632638c33f88d92523fed4df45711bf780
Bethany <26800016+bethanynve@users.noreply.github.com>
2023-10-30 12:09:11 -0400

    Add Codewars 8kyu

---
+++ sql_basics_simple_distinct.sql
@@ -0,0 +1,9 @@
+-- Codewars 8kyu
+-- SQL Basics: Simple DISTINCT
+-- For this challenge you need to create a simple DISTINCT statement, you want to find all the unique ages.
+--people table schema
+-- id, name, age
+-- select table schema
+-- age (distinct)
+
+SELECT DISTINCT age FROM people;
